60c5df6b112b569ebd89e3d5ebcf2c117e225ed6
Dani Cupples <danielle.volker@gmail.com>
2018-01-26 15:49:12 -0500

    test styling added on 2/3 pages

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -19,7 +19,13 @@ app.set("view engine", "ejs");
 var campgrounds = [
   {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5cedc6b95f731395da7269d2341f9a5e&auto=format&fit=crop&w=1050&q=80"},
   {name: "Granite Hill", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89f8de1875d71adbd5fff976ac2341be&auto=format&fit=crop&w=967&q=80"},
-  {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1499363145340-41a1b6ed3630?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b3761b04cf7c21b831647fbef86ad438&auto=format&fit=crop&w=1050&q=80"}
+  {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1499363145340-41a1b6ed3630?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b3761b04cf7c21b831647fbef86ad438&auto=format&fit=crop&w=1050&q=80"},
+  {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5cedc6b95f731395da7269d2341f9a5e&auto=format&fit=crop&w=1050&q=80"},
+  {name: "Granite Hill", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89f8de1875d71adbd5fff976ac2341be&auto=format&fit=crop&w=967&q=80"},
+  {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1499363145340-41a1b6ed3630?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b3761b04cf7c21b831647fbef86ad438&auto=format&fit=crop&w=1050&q=80"},
+  {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5cedc6b95f731395da7269d2341f9a5e&auto=format&fit=crop&w=1050&q=80"},
+  {name: "Granite Hill", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89f8de1875d71adbd5fff976ac2341be&auto=format&fit=crop&w=967&q=80"},
+  {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1499363145340-41a1b6ed3630?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b3761b04cf7c21b831647fbef86ad438&auto=format&fit=crop&w=1050&q=80"},
 ];
 
 app.get("/", function(req, res){
